ea9e71c6d1808d9ea4a18c18405940acaf44bf27
Artem Kharitonov <artem@sysert.ru>
2015-01-15 06:00:59 +0500

    udp_output: handle EAGAIN

--- modules/udp/output.c
+++ modules/udp/output.c
@@ -107,8 +107,18 @@ static void on_ts(module_data_t *mod, const uint8_t *ts)
 
     if(mod->packet.skip > UDP_BUFFER_SIZE - TS_PACKET_SIZE)
     {
-        if(asc_socket_sendto(mod->sock, mod->packet.buffer, mod->packet.skip) == -1)
+        ssize_t ret;
+
+        do
+        {
+            ret = asc_socket_sendto(mod->sock
+                                    , mod->packet.buffer
+                                    , mod->packet.skip);
+        } while(ret == -1 && errno == EAGAIN);
+
+        if(ret == -1)
             asc_log_warning(MSG("error on send [%s]"), asc_socket_error());
+
         mod->packet.skip = 0;
     }
 }
