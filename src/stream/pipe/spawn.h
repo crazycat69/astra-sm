94935d84255735b8ee2bfa13f912a4bc21d6fee1
Artem Kharitonov <artem@sysert.ru>
2015-08-17 20:33:26 +0500

    stream/pipe: add process spawning function

---
+++ src/stream/pipe/spawn.h
@@ -0,0 +1,28 @@
+/*
+ * Astra Module: Pipe (Process spawning)
+ *
+ * Copyright (C) 2015, Artem Kharitonov <artem@sysert.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SPAWN_H_
+#define _SPAWN_H_ 1
+
+/*
+ * TODO: move this to core/spawn
+ */
+pid_t pipe_spawn_child(const char *command, int *sin, int *sout, int *serr);
+
+#endif /* _SPAWN_H_ */
