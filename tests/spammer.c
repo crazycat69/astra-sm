7391ed405a829a78a2461aeca2ad08c8774d7ac7
Artem Kharitonov <artem@sysert.ru>
2015-09-12 04:47:36 +0500

    tests/spammer: use WriteFile() on Windows

--- tests/spammer.c
+++ tests/spammer.c
@@ -20,32 +20,56 @@
 #include <astra.h>
 
 /* 256KiB writes */
-#define WRITE_SIZE ((256 * 1024) / TS_PACKET_SIZE)
+#define WRITE_PKTS ((256 * 1024) / TS_PACKET_SIZE)
+
+static const char *unit_list[] = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", NULL };
 
 int main(void)
 {
 #ifdef _WIN32
+    const HANDLE sout = ASC_TO_HANDLE(_get_osfhandle(STDOUT_FILENO));
     setvbuf(stderr, NULL, _IONBF, 0);
 #endif /* _WIN32 */
 
-    uint8_t buffer[WRITE_SIZE][TS_PACKET_SIZE];
+    /* prepare null packets */
+    uint8_t buffer[WRITE_PKTS][TS_PACKET_SIZE];
     for (size_t i = 0; i < ASC_ARRAY_SIZE(buffer); i++)
         memcpy(buffer[i], null_ts, TS_PACKET_SIZE);
 
+    /* spam them until we get an error */
     size_t written = 0;
     time_t last = time(NULL);
+
     while (true)
     {
         const time_t now = time(NULL);
         if (now != last)
         {
-            fprintf(stderr, "written: %.2f MiB\n", written / 1048576.0);
+            double stat = written;
+            const char *units = NULL;
+
+            for (const char **p = unit_list; *p; p++)
+            {
+                units = *p;
+                if (stat < 1024.0)
+                    break;
+
+                stat /= 1024.0;
+            }
+            fprintf(stderr, "written: %.2f %s\n", stat, units);
 
             written = 0;
             last = now;
         }
 
+#ifdef _WIN32
+        DWORD ret = 0;
+        if (!WriteFile(sout, buffer, sizeof(buffer), &ret, NULL))
+            return 1;
+#else /* _WIN32 */
         const ssize_t ret = write(STDOUT_FILENO, buffer, sizeof(buffer));
+#endif /* !_WIN32 */
+
         if (ret != sizeof(buffer))
             return 1;
 
