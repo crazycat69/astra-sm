9aaab16ad69a47d9666ad07d1aaac49377147aea
Artem Kharitonov <artem@sysert.ru>
2015-09-16 04:49:56 +0500

    core/list: use sys/queue.h if available; move inline functions to header

--- src/core/list.c
+++ src/core/list.c
@@ -1,8 +1,9 @@
 /*
- * Astra Core
+ * Astra Core (Linked lists)
  * http://cesbo.com/astra
  *
  * Copyright (C) 2012-2015, Andrey Dyldin <and@cesbo.com>
+ *                    2015, Artem Kharitonov <artem@sysert.ru>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,182 +24,6 @@
 
 #define MSG(_msg) "[core/list] " _msg
 
-#define QMD_TRACE_ELEM(elem)
-#define QMD_TRACE_HEAD(head)
-#define TRACEBUF
-#define TRASHIT(x)
-
-/*
- * Tail queue declarations.
- */
-
-#define TAILQ_HEAD(name, type) \
-    struct name \
-    { \
-        struct type *tqh_first; /* first element */ \
-        struct type **tqh_last; /* addr of last next element */ \
-        TRACEBUF \
-    }
-
-#define TAILQ_HEAD_INITIALIZER(head) { NULL, &(head).tqh_first }
-
-#define TAILQ_ENTRY(type) \
-    struct \
-    { \
-        struct type *tqe_next;  /* next element */ \
-        struct type **tqe_prev; /* address of previous next element */ \
-        TRACEBUF \
-    }
-
-/*
- * Tail queue functions.
- */
-
-#define TAILQ_CONCAT(head1, head2, field) \
-    do { \
-        if(!TAILQ_EMPTY(head2)) \
-        { \
-            *(head1)->tqh_last = (head2)->tqh_first; \
-            (head2)->tqh_first->field.tqe_prev = (head1)->tqh_last; \
-            (head1)->tqh_last = (head2)->tqh_last; \
-            TAILQ_INIT((head2)); \
-            QMD_TRACE_HEAD(head1); \
-            QMD_TRACE_HEAD(head2); \
-        } \
-    } while (0)
-
-#define TAILQ_EMPTY(head) ((head)->tqh_first == NULL)
-
-#define TAILQ_FIRST(head) ((head)->tqh_first)
-
-#define TAILQ_FOREACH(var, head, field) \
-    for((var) = TAILQ_FIRST((head)) \
-        ; (var) \
-        ; (var) = TAILQ_NEXT((var), field))
-
-#define TAILQ_FOREACH_SAFE(var, head, field, tvar) \
-    for((var) = TAILQ_FIRST((head)) \
-        ; (var) && ((tvar) = TAILQ_NEXT((var), field), 1) \
-        ; (var) = (tvar))
-
-#define TAILQ_FOREACH_REVERSE(var, head, headname, field) \
-    for((var) = TAILQ_LAST((head), headname) \
-        ; (var) \
-        ; (var) = TAILQ_PREV((var), headname, field))
-
-#define TAILQ_FOREACH_REVERSE_SAFE(var, head, headname, field, tvar) \
-    for((var) = TAILQ_LAST((head), headname) \
-        ; (var) && ((tvar) = TAILQ_PREV((var), headname, field), 1) \
-        ; (var) = (tvar))
-
-#define TAILQ_INIT(head) \
-    do { \
-        TAILQ_FIRST((head)) = NULL; \
-        (head)->tqh_last = &TAILQ_FIRST((head)); \
-        QMD_TRACE_HEAD(head); \
-    } while (0)
-
-#define TAILQ_INSERT_AFTER(head, listelm, elm, field) \
-    do { \
-        if ((TAILQ_NEXT((elm), field) = TAILQ_NEXT((listelm), field)) != NULL) \
-            TAILQ_NEXT((elm), field)->field.tqe_prev = &TAILQ_NEXT((elm), field); \
-        else \
-        { \
-            (head)->tqh_last = &TAILQ_NEXT((elm), field); \
-            QMD_TRACE_HEAD(head); \
-        } \
-        TAILQ_NEXT((listelm), field) = (elm); \
-        (elm)->field.tqe_prev = &TAILQ_NEXT((listelm), field); \
-        QMD_TRACE_ELEM(&(elm)->field); \
-        QMD_TRACE_ELEM(&listelm->field); \
-    } while (0)
-
-#define TAILQ_INSERT_BEFORE(listelm, elm, field) \
-    do { \
-        (elm)->field.tqe_prev = (listelm)->field.tqe_prev; \
-        TAILQ_NEXT((elm), field) = (listelm); \
-        *(listelm)->field.tqe_prev = (elm); \
-        (listelm)->field.tqe_prev = &TAILQ_NEXT((elm), field); \
-        QMD_TRACE_ELEM(&(elm)->field); \
-        QMD_TRACE_ELEM(&listelm->field); \
-    } while (0)
-
-#define TAILQ_INSERT_HEAD(head, elm, field) \
-    do { \
-        if ((TAILQ_NEXT((elm), field) = TAILQ_FIRST((head))) != NULL) \
-            TAILQ_FIRST((head))->field.tqe_prev = &TAILQ_NEXT((elm), field); \
-        else \
-            (head)->tqh_last = &TAILQ_NEXT((elm), field); \
-        TAILQ_FIRST((head)) = (elm); \
-        (elm)->field.tqe_prev = &TAILQ_FIRST((head)); \
-        QMD_TRACE_HEAD(head); \
-        QMD_TRACE_ELEM(&(elm)->field); \
-    } while (0)
-
-#define TAILQ_INSERT_TAIL(head, elm, field) \
-    do { \
-        TAILQ_NEXT((elm), field) = NULL; \
-        (elm)->field.tqe_prev = (head)->tqh_last; \
-        *(head)->tqh_last = (elm); \
-        (head)->tqh_last = &TAILQ_NEXT((elm), field); \
-        QMD_TRACE_HEAD(head); \
-        QMD_TRACE_ELEM(&(elm)->field); \
-    } while (0)
-
-#define TAILQ_LAST(head, headname) \
-    (*(((struct headname *)((head)->tqh_last))->tqh_last))
-
-#define TAILQ_NEXT(elm, field) ((elm)->field.tqe_next)
-
-#define TAILQ_PREV(elm, headname, field) \
-    (*(((struct headname *)((elm)->field.tqe_prev))->tqh_last))
-
-#define TAILQ_REMOVE(head, elm, field) \
-    do { \
-        if ((TAILQ_NEXT((elm), field)) != NULL) \
-            TAILQ_NEXT((elm), field)->field.tqe_prev = (elm)->field.tqe_prev; \
-        else \
-        { \
-            (head)->tqh_last = (elm)->field.tqe_prev; \
-            QMD_TRACE_HEAD(head); \
-        } \
-        *(elm)->field.tqe_prev = TAILQ_NEXT((elm), field); \
-        TRASHIT((elm)->field.tqe_next); \
-        TRASHIT((elm)->field.tqe_prev); \
-        QMD_TRACE_ELEM(&(elm)->field); \
-    } while (0)
-
-#define TAILQ_SWAP(head1, head2, type, field) \
-    do { \
-        struct type *swap_first = (head1)->tqh_first; \
-        struct type **swap_last = (head1)->tqh_last; \
-        (head1)->tqh_first = (head2)->tqh_first; \
-        (head1)->tqh_last = (head2)->tqh_last; \
-        (head2)->tqh_first = swap_first; \
-        (head2)->tqh_last = swap_last; \
-        if ((swap_first = (head1)->tqh_first) != NULL) \
-            swap_first->field.tqe_prev = &(head1)->tqh_first; \
-        else \
-            (head1)->tqh_last = &(head1)->tqh_first; \
-        if ((swap_first = (head2)->tqh_first) != NULL) \
-            swap_first->field.tqe_prev = &(head2)->tqh_first; \
-        else \
-            (head2)->tqh_last = &(head2)->tqh_first; \
-    } while (0)
-
-typedef struct asc_item_s
-{
-    void *data;
-    TAILQ_ENTRY(asc_item_s) entries;
-} asc_item_t;
-
-struct asc_list_t
-{
-    size_t size;
-    struct asc_item_s *current;
-    TAILQ_HEAD(list_head_s, asc_item_s) list;
-};
-
 asc_list_t *asc_list_init(void)
 {
     asc_list_t *const list = (asc_list_t *)calloc(1, sizeof(*list));
@@ -213,38 +38,6 @@ void asc_list_destroy(asc_list_t *list)
 {
     asc_assert(list->current == NULL, MSG("list is not empty"));
     free(list);
-}
-
-__asc_inline
-void asc_list_first(asc_list_t *list)
-{
-    list->current = TAILQ_FIRST(&list->list);
-}
-
-__asc_inline
-void asc_list_next(asc_list_t *list)
-{
-    if(list->current)
-        list->current = TAILQ_NEXT(list->current, entries);
-}
-
-__asc_inline
-bool asc_list_eol(const asc_list_t *list)
-{
-    return (list->current == NULL);
-}
-
-__asc_inline
-void *asc_list_data(const asc_list_t *list)
-{
-    asc_assert(list->current != NULL, "[core/list] failed to get data");
-    return list->current->data;
-}
-
-__asc_inline
-size_t asc_list_size(const asc_list_t *list)
-{
-    return list->size;
 }
 
 void asc_list_insert_head(asc_list_t *list, void *data)
--- src/core/list.h
+++ src/core/list.h
@@ -1,8 +1,9 @@
 /*
- * Astra Core
+ * Astra Core (Linked lists)
  * http://cesbo.com/astra
  *
  * Copyright (C) 2012-2013, Andrey Dyldin <and@cesbo.com>
+ *                    2015, Artem Kharitonov <artem@sysert.ru>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,16 +26,26 @@
 #   error "Please include <astra.h> first"
 #endif /* !_ASTRA_H_ */
 
-typedef struct asc_list_t asc_list_t;
+#ifdef HAVE_SYS_QUEUE_H
+#   include <sys/queue.h>
+#endif
+#include "compat_queue.h"
+
+typedef struct asc_item_s
+{
+    void *data;
+    TAILQ_ENTRY(asc_item_s) entries;
+} asc_item_t;
+
+typedef struct
+{
+    size_t size;
+    struct asc_item_s *current;
+    TAILQ_HEAD(list_head_s, asc_item_s) list;
+} asc_list_t;
 
 asc_list_t *asc_list_init(void) __wur;
 void asc_list_destroy(asc_list_t *list);
-
-void asc_list_first(asc_list_t *list);
-void asc_list_next(asc_list_t *list);
-bool asc_list_eol(const asc_list_t *list) __wur;
-void *asc_list_data(const asc_list_t *list) __wur;
-size_t asc_list_size(const asc_list_t *list) __wur;
 
 void asc_list_insert_head(asc_list_t *list, void *data);
 void asc_list_insert_tail(asc_list_t *list, void *data);
@@ -43,6 +54,37 @@ void asc_list_remove_current(asc_list_t *list);
 void asc_list_remove_item(asc_list_t *list, const void *data);
 
 #define asc_list_for(__list) \
-    for(asc_list_first(__list); !asc_list_eol(__list); asc_list_next(__list))
+    for (asc_list_first(__list); !asc_list_eol(__list); asc_list_next(__list))
 
+static inline
+void asc_list_first(asc_list_t *list)
+{
+    list->current = TAILQ_FIRST(&list->list);
+}
+
+static inline
+void asc_list_next(asc_list_t *list)
+{
+    if(list->current)
+        list->current = TAILQ_NEXT(list->current, entries);
+}
+
+static inline __wur
+bool asc_list_eol(const asc_list_t *list)
+{
+    return (list->current == NULL);
+}
+
+static inline __wur
+void *asc_list_data(const asc_list_t *list)
+{
+    asc_assert(list->current != NULL, "[core/list] failed to get data");
+    return list->current->data;
+}
+
+static inline __wur
+size_t asc_list_size(const asc_list_t *list)
+{
+    return list->size;
+}
 #endif /* _ASC_LIST_H_ */
