310391d914c8c7628b4cf162caea8dc9432a9ed6
Artem Kharitonov <artem@3phase.pw>
2016-05-20 17:28:28 +0500

    scripts: add t2mi:// protocol

--- scripts/base.lua
+++ scripts/base.lua
@@ -869,8 +869,86 @@ init_input_module.pipe = function(conf)
     return pipe_generic(conf)
 end
 
-kill_input_module.pipe = function(module)
+kill_input_module.pipe = function(module, conf)
     --
+end
+
+--
+-- Input: t2mi://
+--
+
+t2mi_input_instance_list = {}
+
+function make_t2mi_decap(conf)
+    if conf.name == nil then
+        error("[make_t2mi_decap] option 'name' is required")
+    end
+    if conf.input == nil then
+        error("[make_t2mi_decap] option 'input' is required")
+    end
+
+    local instance = t2mi_input_instance_list[conf.name]
+    if instance ~= nil then
+        return instance
+    end
+
+    local input = parse_url(conf.input)
+    if input == nil then
+        error("[make_t2mi_decap] wrong input format")
+    end
+    input.name = conf.name
+
+    instance = {
+        name = conf.name,
+        input = input,
+        conf = {
+            name = conf.name,
+            pnr = conf.pnr,
+            pid = conf.pid,
+            plp = conf.plp,
+        },
+        clients = 0,
+    }
+
+    return instance
+end
+
+init_input_module.t2mi = function(conf)
+    local instance = _G[conf.addr]
+    local function check_def()
+        if not instance then return false end
+        if not instance.name then return false end
+        if not instance.input then return false end
+        if not instance.conf then return false end
+        return true
+    end
+    if not check_def() then
+        error("[" .. conf.name .. "] t2mi decap definition not found")
+    end
+    local instance_id = instance.name
+
+    if instance.clients == 0 then
+        instance.source = init_input(instance.input)
+        instance.conf.upstream = instance.source.tail:stream()
+        instance.t2mi = t2mi_decap(instance.conf)
+
+        t2mi_input_instance_list[instance_id] = instance
+    end
+
+    instance.clients = instance.clients + 1
+    return instance.t2mi
+end
+
+kill_input_module.t2mi = function(module, conf)
+    local instance_id = module.__options.name
+    local instance = t2mi_input_instance_list[instance_id]
+
+    instance.clients = instance.clients - 1
+    if instance.clients == 0 then
+        instance.t2mi = nil
+        kill_input(instance.source)
+        t2mi_input_instance_list[instance_id] = nil
+    end
 end
 
 -- ooooo         oooooooooo  ooooooooooo ooooo         ooooooo      o      ooooooooo
