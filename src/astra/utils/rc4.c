5cdbfe73331fab73f56d185e5382cb5299f86f6e
Artem Kharitonov <artem@3phase.pw>
2017-09-01 01:03:21 +0500

    utils/rc4: accept (void *) and size_t as arguments

--- src/astra/utils/rc4.c
+++ src/astra/utils/rc4.c
@@ -1,4 +1,22 @@
 /*
+ * Astra Utils (RC4)
+ * http://cesbo.com/astra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * RC4
  * Copyright (c) 1996-2000 Whistle Communications, Inc.
  */
@@ -9,44 +27,40 @@
 static inline
 void swap_bytes(uint8_t *a, uint8_t *b)
 {
-    uint8_t temp;
-    temp = *a;
+    const uint8_t temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void au_rc4_init(rc4_ctx_t *state, const uint8_t *key, int keylen)
+void au_rc4_init(rc4_ctx_t *ctx, const uint8_t *key, size_t keylen)
 {
-    uint8_t j;
-    int i;
+    for (unsigned int i = 0; i < 256; i++)
+        ctx->perm[i] = i;
 
-    for(i = 0; i < 256; i++)
-        state->perm[i] = (uint8_t)i;
+    ctx->index1 = 0;
+    ctx->index2 = 0;
 
-    state->index1 = 0;
-    state->index2 = 0;
-
-    for(j = i = 0; i < 256; i++)
+    uint8_t j = 0;
+    for (unsigned int i = 0; i < 256; i++)
     {
-        j += state->perm[i] + key[i % keylen];
-        swap_bytes(&state->perm[i], &state->perm[j]);
+        j += ctx->perm[i] + key[i % keylen];
+        swap_bytes(&ctx->perm[i], &ctx->perm[j]);
     }
 }
 
-void au_rc4_crypt(rc4_ctx_t *state, uint8_t *dst, const uint8_t *buf
-                  , int buflen)
+void au_rc4_crypt(rc4_ctx_t *ctx, void *dst, const void *src, size_t len)
 {
-    int i;
-    uint8_t j;
+    const uint8_t *const srcp = (uint8_t *)src;
+    uint8_t *const dstp = (uint8_t *)dst;
 
-    for (i = 0; i < buflen; i++)
+    for (size_t i = 0; i < len; i++)
     {
-        state->index1++;
-        state->index2 += state->perm[state->index1];
+        ctx->index1++;
+        ctx->index2 += ctx->perm[ctx->index1];
 
-        swap_bytes(&state->perm[state->index1], &state->perm[state->index2]);
+        swap_bytes(&ctx->perm[ctx->index1], &ctx->perm[ctx->index2]);
 
-        j = state->perm[state->index1] + state->perm[state->index2];
-        dst[i] = buf[i] ^ state->perm[j];
+        const uint8_t j = ctx->perm[ctx->index1] + ctx->perm[ctx->index2];
+        dstp[i] = srcp[i] ^ ctx->perm[j];
     }
 }
